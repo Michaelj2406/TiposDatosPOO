e5e458962f2e8985cfa63dce538d87b8f4e99409
Michaelj2406 <michael.chasiguano734@ist17dejulio.edu.ec>
2024-05-08 14:23:51 -0500

    Tipos de datos y Casting usuando entrada mediante Scanner Y salidas mediante con sout

--- src/vista/Main.java
+++ src/vista/Main.java
@@ -4,10 +4,50 @@
  */
 package vista;
 
+import java.util.Scanner;
+
 /**
  *
  * @author LENOVO
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        //TIPO DE DATO
+        //1.- ENTEROS
+        //DECLARO VALORES
+ //        int numeroUno=10; //Publica 
+ //        int numeroDos=8;
+         //2.-DECLARO EN 0
+         //SOBREESCRIBIR VALORES
+         int numeroUno=8; //Publica 
+         int numeroDos=19;
+         //1.- SUMA 2 NÚMEROS
+         int resultado=numeroUno+numeroDos;
+        
+         //IMPRIMIR SALIDA DE INFORMACIÓN
+         System.out.println("El resultado es: "+resultado);
+         
+         //3.- SUMA 2 NÚMEROS, UTILIZAR ENTRADAS Y SALIDAS
+         Scanner s=new Scanner(System.in);
+         //TRANSFORMAR DE UN OBJETO CADENA A UN ENTERO
+         System.out.println("Ingrese número 1: ");
+         int num1=Integer.parseInt(s.next()); //int num1=(s.nextInt());
+         System.out.println("Ingrese número 2: ");
+         int num2=s.nextInt();
+         //SOBREESCRITURA
+         num1+=num2; // num1=num1+=num2 = resp=num1+num2
+         System.out.println("El resultado es: "+num1);
+         
+         //tRANSFORMANDO DE CADENA A ENTERO
+         //1.- CADENA = CONJUNTO {} DE CARACTERES
+         String cadena="La suma de de 2 números enteros es: ";
+          System.out.println("Ingrese número 1: ");
+         int n1=Integer.parseInt(s.next()); //int num1=(s.nextInt());
+         System.out.println("Ingrese número 2: ");
+         int n2=s.nextInt();
+         //DOUBLE RECIBE TANTO ENTEROS COMO DECIMALES
+         double resul=n1+n2;
+         //+"" Transforma cualquier tipo de dato en cadena
+         cadena+=resul+""; //Concateno
+    }
 }
